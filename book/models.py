56a181a3d60628690c9037fa9e760d8a159b7ce4
Adeliyassss <100111844+Adeliyassss@users.noreply.github.com>
2023-10-16 02:19:39 +0600

    2,3

--- book/models.py
+++ book/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=30)
+    POPULARITY = (
+        ('Актуален', 'Актуален'),
+        ('50 на 50', '50 на 50'),
+        ('Стрем', 'Стрем'),
+    )
+    title = models.CharField(max_length=40)
     description = models.TextField()
     image = models.ImageField(upload_to='')
+    popularity = models.CharField(max_length=100, choices=POPULARITY, default=POPULARITY[0], null=True)
+    video = models.URLField(null=True)
     cost = models.CharField(max_length=10)
     created_date = models.DateTimeField(auto_now_add=True)
 
