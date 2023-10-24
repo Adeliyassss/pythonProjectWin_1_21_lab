c0d24ed48722f884bf76a8751a5f517f0a3f5563
Adeliyassss <100111844+Adeliyassss@users.noreply.github.com>
2023-10-25 00:34:45 +0600

    comments

---
+++ book/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from . import models
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = models.ReviewBook
+        fields = '__all__'
+
--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
 
 def book_view(request):
     book_value = models.Book.objects.all()
@@ -8,3 +9,14 @@ def book_view(request):
 def book_detail_view(request, id):
     book_id = get_object_or_404(models.Book, id=id)
     return render(request, 'book_detail.html', {'book_key': book_id})
+
+def createBookView(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('<h1>Комментарий успешно добавлен</h1>')
+    else:
+        form = forms.ReviewForm()
+    return render(request, 'create_review.html', {'form': form})
